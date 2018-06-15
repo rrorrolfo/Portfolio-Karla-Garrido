a2df324cec855c4de86ee67fe32c3ff1a1241cbd
Rodolfo Perez <rodolfobeste@gmail.com>
2018-06-15 16:48:25 +0300

    improved code presentation in js

--- js/apps.js
+++ js/apps.js
@@ -1,43 +1,40 @@
-const teatro = document.querySelector("#menu_teatro");
 const boton = document.getElementsByClassName("boton_teatro");
 const trayectoria = document.querySelector("#trayectoria_teatro");
 const trayectoria_cine = document.querySelector("#trayectoria_cine");
 const trayectoria_television = document.querySelector("#trayectoria_television");
 
-
-
-boton[0].addEventListener('click', () => {    
-                       if (trayectoria.style.display == 'none') {
-                boton[0].textContent = 'Esconder trayectoria en teatro';
-            trayectoria.style.display = 'block';
+boton[0].addEventListener('click', (event) => {
+   
+  if (trayectoria.style.display == 'none') {
+    trayectoria.style.display = 'block';
+    boton[0].textContent = 'Esconder trayectoria en teatro';
+            
   } else {
     boton[0].textContent = 'Mostrar trayectoria en teatro';
-  trayectoria.style.display = 'none';
-}
- });
+    trayectoria.style.display = 'none';
+  }
+});
 
 boton[1].addEventListener("click", () => {    
                 
-                       if (trayectoria_cine.style.display == "none") {
-  boton[1].textContent = "Esconder trayectoria en cine";
-  trayectoria_cine.style.display = "block";
-} else {
-  boton[1].textContent = 'Mostrar trayectoria en cine';
-  trayectoria_cine.style.display = 'none'; 
-}
-
- });
+  if (trayectoria_cine.style.display == "none") {
+    boton[1].textContent = "Esconder trayectoria en cine";
+    trayectoria_cine.style.display = "block";
+  } else {
+    boton[1].textContent = 'Mostrar trayectoria en cine';
+    trayectoria_cine.style.display = 'none'; 
+  }
+});
 
 boton[2].addEventListener("click", () => {    
                 
-                       if (trayectoria_television.style.display == "none") {
+  if (trayectoria_television.style.display == "none") {
   boton[2].textContent = "Esconder trayectoria en televisión";
   trayectoria_television.style.display = "block";
-} else {
+  } else {
   boton[2].textContent = 'Mostrar trayectoria en television';
   trayectoria_television.style.display = 'none'; 
-}
-
- });
+  }
+});
 
 
