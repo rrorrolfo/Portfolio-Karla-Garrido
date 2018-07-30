276ca00a42089fa568812993b07bb26f9cebcebe
Rodolfo Perez <rodolfobeste@gmail.com>
2018-07-30 15:42:17 +0300

    Added menu for mobile, fixed bugs for js trayectoria section

--- js/apps.js
+++ js/apps.js
@@ -2,10 +2,21 @@ const boton = document.getElementsByClassName("boton_teatro");
 const trayectoria = document.querySelector("#trayectoria_teatro");
 const trayectoria_cine = document.querySelector("#trayectoria_cine");
 const trayectoria_television = document.querySelector("#trayectoria_television");
+const menu = document.querySelector("#menu");
+const mobile_nav = document.querySelector(".nav-container");
+
+menu.addEventListener("click", () => {
+
+  if(mobile_nav.style.display == "none" || mobile_nav.style.display == "") {
+  mobile_nav.style.display = "block";
+  } else {
+    mobile_nav.style.display = "none";
+  }
+});
 
 boton[0].addEventListener('click', (event) => {
    
-  if (trayectoria.style.display == 'none') {
+  if (trayectoria.style.display == 'none' || trayectoria.style.display == '') {
     trayectoria.style.display = 'block';
     boton[0].textContent = 'Esconder trayectoria en teatro';
             
@@ -17,7 +28,7 @@ boton[0].addEventListener('click', (event) => {
 
 boton[1].addEventListener("click", () => {    
                 
-  if (trayectoria_cine.style.display == "none") {
+  if (trayectoria_cine.style.display == "none" || trayectoria_cine.style.display == '') {
     boton[1].textContent = "Esconder trayectoria en cine";
     trayectoria_cine.style.display = "block";
   } else {
@@ -28,7 +39,7 @@ boton[1].addEventListener("click", () => {
 
 boton[2].addEventListener("click", () => {    
                 
-  if (trayectoria_television.style.display == "none") {
+  if (trayectoria_television.style.display == "none" || trayectoria_television.style.display == '') {
   boton[2].textContent = "Esconder trayectoria en televisión";
   trayectoria_television.style.display = "block";
   } else {
